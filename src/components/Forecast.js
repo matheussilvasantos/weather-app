a7ab12259b985146e50acc22f7f51f030823b3c5
Matheus Silva Santos de Oliveira <oliveira.matheussilva@outlook.com>
2017-06-18 20:08:58 -0300

    Show loading while search for weather

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
+
 import Weathers from "./Weathers";
+import Loading from "./Loading";
+
 import api from "../utils/api";
+
 import queryString from "query-string";
+
+import PropTypes from "prop-types";
+
+function RenderWeathers(props) {
+  return(
+    <div>
+      <h1>{props.city.name}</h1>
+      <Weathers weathers={props.weathers} />
+    </div>
+  )
+}
+
+RenderWeathers.propTypes = {
+  city: PropTypes.object.isRequired,
+  weathers: PropTypes.array.isRequired
+}
 
 class Forecast extends Component {
   constructor(props) {
@@ -24,14 +44,11 @@ class Forecast extends Component {
       }.bind(this));
   }
   render() {
-    var location = queryString.parse(this.props.location.search);
     return (
       <div>
-        {this.state.weathers &&
-          <div>
-            <h1>{this.state.city.name}</h1>
-            <Weathers weathers={this.state.weathers} />
-          </div>}
+        {this.state.weathers
+            ? <RenderWeathers city={this.state.city} weathers={this.state.weathers} />
+            : <Loading />}
       </div>
     );
   }
