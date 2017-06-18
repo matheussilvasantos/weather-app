43a0ba89dc4fa32d50b923b6ed3aba1be724fff7
Matheus Silva Santos de Oliveira <oliveira.matheussilva@outlook.com>
2017-06-18 20:22:22 -0300

    Remove warnings
    Missing a key on li elements and alt attribute on img tag.

--- src/components/Weathers.js
+++ src/components/Weathers.js
@@ -2,22 +2,23 @@ import React from "react";
 import dateFormat from "dateformat";
 
 function Weather(props) {
+  let icon = process.env.PUBLIC_URL + "/weather-icons/" + props.weather.icon + ".svg";
   return(
     <div>
-      <img src={process.env.PUBLIC_URL + "/weather-icons/" + props.weather.icon + ".svg"} />
+      <img src={icon} alt={props.weather.main} />
       <span>{dateFormat(props.now, "dddd, mmmm d")}</span>
     </div>
   )
 }
 
-function Weathers(props)  {
+function Weathers(props) {
   let now = new Date();
   return (
     <div>
       <ul>
         {props.weathers.map(function(weather, index) {
           return (
-            <li>
+            <li key={index}>
               <Weather weather={weather.weather[0]} now={now.setDate(now.getDate() + index)} />
             </li>
           )
